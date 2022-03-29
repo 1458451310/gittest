c1a6755640a43ea86ca217576a562930582c9d97
wx <wx@qq.com>
2022-03-29 11:33:56 +0800

    master commit1

--- src/main/java/com/wx/Hello.java
+++ src/main/java/com/wx/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git3");
+        System.out.println("hello git3 master");
     }
 
 }
